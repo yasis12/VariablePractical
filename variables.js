6515f596e1d3d307a94e6fc0f842878b644983e5
Rafael Yasis <rafaelyasis@gmail.com>
2023-06-11 10:31:49 -0500

    Unit 2 video follow along

---
+++ variables.js
@@ -0,0 +1,11 @@
+console.log( 'js' );
+
+let testVar = 9;
+
+console.log(testVar);
+
+console.log( 'the value of testVar is', testVar);
+
+testVar = 1234
+
+console.log( 'the value of testVar is', testVar);
